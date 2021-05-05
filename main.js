6184be0639ea53d7dde24c193734275cc369f254
Shoter99 <shoter998@gmail.com>
2021-05-05 09:46:39 +0200

    Added button to switch camera on/off

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 const video = document.getElementById("video")
+const btn = document.querySelector("button")
+var isOn = false
+
+btn.addEventListener('click', enableVideo)
+
+function enableVideo(){
+    if(isOn){
+        const canvas = document.querySelector("canvas")
+        video.style.display="none"
+        video.pause()
+
+    }
+    else{
+        
+        video.style.display="flex"
+        video.play()
+    }
+    isOn = !isOn
+}
+
 
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
@@ -17,7 +37,7 @@ function startVideo(){
 
 video.addEventListener('play', () => {
     const canvas = faceapi.createCanvasFromMedia(video)
-    document.body.append(canvas)
+    document.getElementById("video-wrapper").append(canvas)
     const displaySize = {width: video.width, height: video.height}
     faceapi.matchDimensions(canvas, displaySize)
     setInterval(async () =>{
@@ -26,9 +46,14 @@ video.addEventListener('play', () => {
             .TinyFaceDetectorOptions())
             .withFaceLandmarks()
             .withFaceExpressions()
-            
+            console.log(detections)
             const resizedDetections = faceapi.resizeResults(detections, displaySize)
             canvas.getContext('2d').clearRect(0,0,canvas.width, canvas.height)
             faceapi.draw.drawDetections(canvas, resizedDetections)
-    }, 100)
+            faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    }, 50)
+})
+video.addEventListener('pause', () => {
+    const canvas = document.querySelector('canvas')
+    canvas.remove()
 })
